ed50a5f196d5d10d81502a12e0b3b9c384c37933
Samuel Diogo <samudiogo@gmail.com>
2018-09-06 17:49:46 -0300

    implementação da personController do meu jeito

--- RestWithAspnetCoreUdemy/Controllers/PersonController.cs
+++ RestWithAspnetCoreUdemy/Controllers/PersonController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using RestWithAspnetCoreUdemy.Model;
+using RestWithAspnetCoreUdemy.Services;
 
 namespace RestWithAspnetCoreUdemy.Controllers
 {
@@ -11,5 +9,53 @@ namespace RestWithAspnetCoreUdemy.Controllers
     [ApiController]
     public class PersonController : ControllerBase
     {
+        private readonly IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        // GET: api/Person
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        // GET: api/Person/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var person = _personService.FindByid(id);
+            if (person == null) return BadRequest("usuário não encontrado");
+            return Ok(person);
+        }
+
+        // POST: api/Person
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest("invalid object");
+
+            return new OkObjectResult(_personService.Create(person));
+        }
+
+        // PUT: api/Person/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest("invalid object");
+
+            return new OkObjectResult(_personService.Update(person));
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
     }
 }
--- RestWithAspnetCoreUdemy/Startup.cs
+++ RestWithAspnetCoreUdemy/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using RestWithAspnetCoreUdemy.Services;
 
 namespace RestWithAspnetCoreUdemy
 {
@@ -26,6 +20,8 @@ namespace RestWithAspnetCoreUdemy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddScoped<IPersonService, PersonService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
